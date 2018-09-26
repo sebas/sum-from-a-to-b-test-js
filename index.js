8bb91ec35f0f8283ed322070d911c3dbc727531d
Sebastian Mantilla <sebastian.mantilla@mail.com>
2018-09-26 16:26:43 -0700

    Sum is now working recursively

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 
 function sum(fromN, toN) {
+  // console.log(`De ${fromN} a ${toN}`);
   // Sum all the values from fromN up to toN
+  if (fromN === toN) {
+    return toN;
+  }
+
+  return fromN + sum(fromN+1, toN);
 }
 
 module.exports = sum;
